4a64904bd4c80135803c3ed08c6d6fb90b7402df
huangww01 <huangww01@mingyuanyun.com>
2023-10-26 09:18:31 +0800

    添加pb编译后的客户端代码

--- abi/build.rs
+++ abi/build.rs
@@ -1,12 +1,10 @@
 use std::io::Result;
-use std::{fs, process::Command};
+use std::process::Command;
 
 fn main() -> Result<()> {
     tonic_build::configure()
         .out_dir("src/pb")
-        .build_client(false)
         .compile(&["protos/reservation.proto"], &["protos"])?;
-    // fs::remove_file("src/pb/google.protobuf.rs")?;
 
     Command::new("cargo").args(&["fmt"]).output().unwrap();
 
--- abi/src/pb/reservation.rs
+++ abi/src/pb/reservation.rs
@@ -256,6 +256,250 @@ impl ReservationUpdateType {
         }
     }
 }
+/// Generated client implementations.
+pub mod reservation_service_client {
+    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    use tonic::codegen::http::Uri;
+    use tonic::codegen::*;
+    /// Reservation service
+    #[derive(Debug, Clone)]
+    pub struct ReservationServiceClient<T> {
+        inner: tonic::client::Grpc<T>,
+    }
+    impl ReservationServiceClient<tonic::transport::Channel> {
+        /// Attempt to create a new client by connecting to a given endpoint.
+        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+        where
+            D: TryInto<tonic::transport::Endpoint>,
+            D::Error: Into<StdError>,
+        {
+            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
+            Ok(Self::new(conn))
+        }
+    }
+    impl<T> ReservationServiceClient<T>
+    where
+        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T::Error: Into<StdError>,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+    {
+        pub fn new(inner: T) -> Self {
+            let inner = tonic::client::Grpc::new(inner);
+            Self { inner }
+        }
+        pub fn with_origin(inner: T, origin: Uri) -> Self {
+            let inner = tonic::client::Grpc::with_origin(inner, origin);
+            Self { inner }
+        }
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> ReservationServiceClient<InterceptedService<T, F>>
+        where
+            F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
+            T: tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+                Response = http::Response<
+                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                >,
+            >,
+            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
+                Into<StdError> + Send + Sync,
+        {
+            ReservationServiceClient::new(InterceptedService::new(inner, interceptor))
+        }
+        /// Compress requests with the given encoding.
+        ///
+        /// This requires the server to support it otherwise it might respond with an
+        /// error.
+        #[must_use]
+        pub fn send_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.send_compressed(encoding);
+            self
+        }
+        /// Enable decompressing responses.
+        #[must_use]
+        pub fn accept_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.accept_compressed(encoding);
+            self
+        }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
+        /// make a reservation
+        pub async fn reserve(
+            &mut self,
+            request: impl tonic::IntoRequest<super::ReserveRequest>,
+        ) -> std::result::Result<tonic::Response<super::ReserveResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/reserve");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "reserve"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// confirm a pending reservation, if reservation is not pending, do nothing
+        pub async fn confirm(
+            &mut self,
+            request: impl tonic::IntoRequest<super::ConfirmRequest>,
+        ) -> std::result::Result<tonic::Response<super::ConfirmResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/confirm");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "confirm"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// update the reservation note
+        pub async fn update(
+            &mut self,
+            request: impl tonic::IntoRequest<super::UpdateRequest>,
+        ) -> std::result::Result<tonic::Response<super::UpdateResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/update");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "update"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// cancel a reservation
+        pub async fn cancel(
+            &mut self,
+            request: impl tonic::IntoRequest<super::CancelRequest>,
+        ) -> std::result::Result<tonic::Response<super::CancelResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/cancel");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "cancel"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// get a reservation by id
+        pub async fn get(
+            &mut self,
+            request: impl tonic::IntoRequest<super::GetRequest>,
+        ) -> std::result::Result<tonic::Response<super::GetResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static("/reservation.ReservationService/get");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "get"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// query reservations by resource id, user id, status, start time, end time
+        pub async fn query(
+            &mut self,
+            request: impl tonic::IntoRequest<super::QueryRequest>,
+        ) -> std::result::Result<
+            tonic::Response<tonic::codec::Streaming<super::Reservation>>,
+            tonic::Status,
+        > {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/query");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "query"));
+            self.inner.server_streaming(req, path, codec).await
+        }
+        /// filter reservations, order by reservation id
+        pub async fn filter(
+            &mut self,
+            request: impl tonic::IntoRequest<super::FilterRequest>,
+        ) -> std::result::Result<tonic::Response<super::FilterResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/filter");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "filter"));
+            self.inner.unary(req, path, codec).await
+        }
+        /// another system could monitor newly added/confirmed/cancelled reservations
+        pub async fn listen(
+            &mut self,
+            request: impl tonic::IntoRequest<super::ListenRequest>,
+        ) -> std::result::Result<
+            tonic::Response<tonic::codec::Streaming<super::Reservation>>,
+            tonic::Status,
+        > {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path =
+                http::uri::PathAndQuery::from_static("/reservation.ReservationService/listen");
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("reservation.ReservationService", "listen"));
+            self.inner.server_streaming(req, path, codec).await
+        }
+    }
+}
 /// Generated server implementations.
 pub mod reservation_service_server {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
